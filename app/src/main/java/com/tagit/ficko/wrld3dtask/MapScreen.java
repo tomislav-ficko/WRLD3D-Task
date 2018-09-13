51ca2e04508c9f79323e7fe8b1a0975949802a04
tficko <tomislav.ficko@infobip.com>
2018-09-13 13:01:44 +0200

    code refactor

--- app/src/main/java/com/tagit/ficko/wrld3dtask/MapScreen.java
+++ app/src/main/java/com/tagit/ficko/wrld3dtask/MapScreen.java
@@ -2,8 +2,10 @@ package com.tagit.ficko.wrld3dtask;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.eegeo.indoors.IndoorMapView;
 import com.eegeo.mapapi.EegeoApi;
 import com.eegeo.mapapi.EegeoMap;
 import com.eegeo.mapapi.MapView;
@@ -14,6 +16,8 @@ public class MapScreen extends AppCompatActivity {
     public static final String API_KEY = "9f3eb59fe643c587896f39d7ad4375ce";
 
     private MapView m_mapView;
+//    private EegeoMap m_eegeoMap = null;
+//    private IndoorMapView m_interiorView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,11 @@ public class MapScreen extends AppCompatActivity {
         m_mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(final EegeoMap map) {
+//                m_eegeoMap = map;
+//
+//                RelativeLayout uiContainer = (RelativeLayout) findViewById(R.id.eegeo_ui_container);
+//                m_interiorView = new IndoorMapView(m_mapView, uiContainer, m_eegeoMap);
+
                 Toast.makeText(MapScreen.this, "Welcome to Eegeo Maps", Toast.LENGTH_LONG).show();
             }
         });
