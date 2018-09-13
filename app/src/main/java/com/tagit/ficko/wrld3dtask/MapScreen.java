67e21688b6d01b41673d11de9c9fac578c67baa8
tficko <tomislav.ficko@infobip.com>
2018-09-13 08:47:22 +0200

    initialize project

---
+++ app/src/main/java/com/tagit/ficko/wrld3dtask/MapScreen.java
@@ -0,0 +1,13 @@
+package com.tagit.ficko.wrld3dtask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_screen);
+    }
+}
